f072153ca06a1781079069e5d500cde1144abb0f
XinChen1020 <xinc1020@gmail.com>
2024-03-29 00:44:39 -0400

    update build.sh
    Change bash path

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/bash
-java -classpath ../../javacc.jar javacc PA5.jj
+#!/bin/bash
+java -classpath javacc.jar javacc PA5.jj
 javac PA5.java
 javac CodeGen_Visitor.java
 echo "compiled java files"
-cp tests/test0.c tests/demo.c
-java PA5 < tests/demo.c > tests/demo.s
-cd tests; gcc demo.s print.s -o demo; ./demo
-
+cp tests/test0.c tests/demo_while.c
+java PA5 < tests/demo_while.c > tests/demo_while.s
+cd tests; gcc demo_while.s print.s -o demo; ./demo
