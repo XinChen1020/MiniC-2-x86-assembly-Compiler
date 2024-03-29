9b7bf04732039754a1eaa2441bd669bb6e4cf1d2
XinChen1020 <xinc1020@gmail.com>
2024-03-29 12:41:14 -0400

    Change output file name

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ java -classpath javacc.jar javacc PA5.jj
 javac PA5.java
 javac CodeGen_Visitor.java
 echo "compiled java files"
-cp tests/test0.c tests/demo_while.c
 java PA5 < tests/demo_while.c > tests/demo_while.s
-cd tests; gcc demo_while.s print.s -o demo; ./demo
+cd tests; gcc demo_while.s print.s -o demo_while; ./demo_while
